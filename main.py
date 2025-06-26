c1f9dc67009e08a895b7519800b63eb1a0bda16b
caseitz <smcaseit@gmail.com>
2025-06-26 22:29:40 +0200

    starting

---
+++ main.py
@@ -0,0 +1,11 @@
+import pygame
+from constants import *
+
+
+def main():
+    return print(f'''Starting Asteroids!
+Screen width: {SCREEN_WIDTH}
+Screen height: {SCREEN_HEIGHT}''')
+
+if __name__ == "__main__":
+    main()
