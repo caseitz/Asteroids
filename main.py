cf171a36e29dde073022297356db72f6a74ae7a1
caseitz <smcaseit@gmail.com>
2025-06-29 21:41:24 +0200

    fps

--- main.py
+++ main.py
@@ -9,6 +9,9 @@ def main():
     print(f'''Starting Asteroids!
 Screen width: {SCREEN_WIDTH}
 Screen height: {SCREEN_HEIGHT}''')
+    clock = pygame.time.Clock() 
+    dt = 0
+
 
     while True:
         for event in pygame.event.get():
@@ -17,6 +20,10 @@ Screen height: {SCREEN_HEIGHT}''')
 
         screen.fill((0, 0, 1))
         pygame.display.flip()
+
+        clock.tick(60)
+
+        dt = (clock.tick())/1000
 
 if __name__ == "__main__":
     main()
